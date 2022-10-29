55b48073b3066cacd38f9456e81a87cd1c1e1189
vvkhil <MVK68-LBJ@yandex.by>
2022-10-30 00:13:28 +0300

    Add drawables and recyclerView for InternalFragment

--- app/src/main/java/com/example/fileexplorer/fragments/InternalFragment.java
+++ app/src/main/java/com/example/fileexplorer/fragments/InternalFragment.java
@@ -1,17 +1,40 @@
 package com.example.fileexplorer.fragments;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.fileexplorer.FileAdapter;
 import com.example.fileexplorer.R;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class InternalFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private FileAdapter fileAdapter;
+    private List<File> fileList;
+    private ImageView img_back;
+    private TextView tv_pathHolder;
+    File storage;
 
     View view;
 
@@ -20,6 +43,68 @@ public class InternalFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_internal, container, false);
 
+        tv_pathHolder = view.findViewById(R.id.tv_pathHolder);
+        img_back = view.findViewById(R.id.img_back);
+
+        String internalStorage = System.getenv("EXTERNAL_STORAGE");
+        storage = new File(internalStorage);
+
+        tv_pathHolder.setText(storage.getAbsolutePath());
+        runtimePermission();
+
         return view;
+    }
+
+    private void runtimePermission() {
+        Dexter.withContext(getContext()).withPermissions(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        ).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                displayFiles();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+    public ArrayList<File> findFiles(File file){
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] files = file.listFiles();
+
+        for(File singleFile : files){
+            if(singleFile.isDirectory() && !singleFile.isHidden()){
+                arrayList.add(singleFile);
+            }
+        }
+        for(File singleFile: files){
+            if(singleFile.getName().toLowerCase().endsWith(".jpeg") ||
+                    singleFile.getName().toLowerCase().endsWith(".jpg") ||
+                    singleFile.getName().toLowerCase().endsWith(".png") ||
+                    singleFile.getName().toLowerCase().endsWith(".mp3") ||
+                    singleFile.getName().toLowerCase().endsWith(".wav") ||
+                    singleFile.getName().toLowerCase().endsWith(".mp4") ||
+                    singleFile.getName().toLowerCase().endsWith(".pdf") ||
+                    singleFile.getName().toLowerCase().endsWith(".doc") ||
+                    singleFile.getName().toLowerCase().endsWith(".apk"))
+            {
+                arrayList.add(singleFile);
+            }
+        }
+        return arrayList;
+    }
+
+    private void displayFiles() {
+        recyclerView = view.findViewById(R.id.recycler_internal);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        fileList = new ArrayList<>();
+        fileList.addAll(findFiles(storage));
+        fileAdapter = new FileAdapter(getContext(), fileList);
+        recyclerView.setAdapter(fileAdapter);
     }
 }
